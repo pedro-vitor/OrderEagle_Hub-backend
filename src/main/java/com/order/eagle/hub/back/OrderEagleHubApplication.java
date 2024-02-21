f09d0d3a873d696ba03f6285a273a981cc85a23e
Pedro Vitor <pedrocoud12@gmail.com>
2024-02-21 15:09:36 -0300

    Projeto criado - configurações do h2

---
+++ src/main/java/com/order/eagle/hub/back/OrderEagleHubApplication.java
@@ -0,0 +1,13 @@
+package com.order.eagle.hub.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEagleHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEagleHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/order/eagle/hub/back/OrderEagleHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.eagle.hub.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEagleHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
